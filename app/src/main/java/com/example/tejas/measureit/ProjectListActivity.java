d0bfa35b911ee5d13de94b431a38be766f15e957
tejas <gajaretejas@gmail.com>
2018-06-30 15:10:51 +0530

    Sample Projects added

--- app/src/main/java/com/example/tejas/measureit/ProjectListActivity.java
+++ app/src/main/java/com/example/tejas/measureit/ProjectListActivity.java
@@ -2,9 +2,23 @@ package com.example.tejas.measureit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectListActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "ProjectListActivity";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    List<Project> projectList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +29,27 @@ public class ProjectListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("My Projects");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        projectList = new ArrayList<>();
+
+
+        for(int i=1; i<=8; i++){
+            Project temp = new Project(
+                    i,
+                    "Project : "+i,
+                    "Address : Hinjewadi",
+                    10+" Measurements",
+                    R.drawable.property);
+
+            projectList.add(temp);
+        }
+
+        mAdapter = new MyAdapter(this, projectList);
+        recyclerView.setAdapter(mAdapter);
     }
 }
